790bc7059397c958086919f58d98d5c96081a48d
navira5 <navira.ali@gmail.com>
2018-10-02 11:44:24 -0500

    add client to canvas

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
-//import styled from 'styled-components';
-//import Client from './components/Client.jsx';
-//import { throws } from 'assert';
-//import Node from './components/Node.jsx';
+import styled from 'styled-components';
+import Client from './Client.jsx';
+import Server from './Server.jsx';
+import Database from './Database.jsx';
+import { throws } from 'assert';
 
-// const Svg = styled.svg`
-//   border: 1px solid #ddd;
-//   width: 100%;
-//   height: 400px;
-// `;
+const Svg = styled.svg`
+  border: 1px solid #ddd;
+  width: 100%;
+  height: 400px;
+`;
 
 
 class Canvas extends Component {
@@ -38,7 +39,7 @@ class Canvas extends Component {
     this.handleNewNode = this.handleNewNode.bind(this);
   }
 
-  //data = {x: val, y: val, type: ''};
+  //data = {x: val, y: val, type: '', routes: [{method: 'get', url: '/clientsomething'}, {method: 'post, url: ''}];
   handleNewNode(data) {
     data.room = this.props.match.params.name;
     this.socket.emit('add node', data);
@@ -51,11 +52,18 @@ class Canvas extends Component {
   }
 
   render() {
+    const showClients = nodes.filter((node, i) => {
+      return node.type === 'client' ? <Client x={node.x} y={node.y}/> : null
+    })
     return (
       <div>
         <button onClick={() => this.handleNewNode({ position: { x: 20, y: 20 }, type: 'client' })}> Client +</button>
-        <button onClick={() => this.handleNewNode({ position: { x: 40, y: 20 }, type: 'server' })}> Server +</button>
-        {this.state.nodes.map((node, i) => <p key={i}>{node.type} at x:{node.position.x} y:{node.position.y}</p>)}
+        <button onClick={() => this.handleNewNode({ position: { x: 250, y: 20 }, type: 'server' })}> Server +</button>
+        <button onClick={() => this.handleNewNode({ position: { x: 350, y: 20 }, type: 'database' })}> Database +</button>
+        {/* {this.state.nodes.map((node, i) => <p key={i}>{node.type} at x:{node.position.x} y:{node.position.y}</p>)} */}
+        <Svg>
+         {showClients}
+        </Svg>
       </div>
     );
   }
