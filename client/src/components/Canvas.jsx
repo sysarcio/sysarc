6b756eb14d542781c8cb4261630fba93eedf4a4f
Tredway91 <Tredway91@users.noreply.github.com>
2018-10-03 16:05:53 -0500

    Feature/backend routes (#33)
    * Implement basic addNode functionality
    
    * Edits for db gui
    
    * Implement addCanvas, and addNode db queries
    
    * Updated addCanvas query to not be hardcoded
    
    * Persist move node and delete node in db
    
    * Reset client.jsx to original drag and drop methods

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -108,9 +108,15 @@ class Canvas extends Component {
         <button onClick={() => this.handleNewNode({ position: { x: 20, y: 20 }, type: 'CLIENT' })}> Client +</button>
         <button onClick={() => this.handleNewNode({ position: { x: 250, y: 20 }, type: 'SERVER' })}> Server +</button>
         <button onClick={() => this.handleNewNode({ position: { x: 350, y: 20 }, type: 'DATABASE' })}> Database +</button>
+<<<<<<< HEAD
+        <Svg>
+          {showClients}
+        </Svg>
+=======
         <svg style={svgStyle}>
           {showClients}
         </svg>
+>>>>>>> 915c0180989920184ef88092510656d79e4e3555
       </div>
     );
   }
