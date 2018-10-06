eba0207a26fb593e25f39a9994ae42d60fcb1443
Mitchel Constantin <mitchconstantin@gmail.com>
2018-10-06 15:45:11 -0500

    Feature/uploads screenshots (#51)
    * adds upload to server button and functionality
    
    * adds package lock json

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -5,6 +5,7 @@ import Server from './Server.jsx';
 import Database from './Database.jsx';
 import { throws } from 'assert';
 import canvg from 'canvg';
+import axios from 'axios';
 // import styled from 'styled-components';
 
 // const Svg = styled.svg`
@@ -50,13 +51,14 @@ class Canvas extends Component {
 
     this.socket.on('route updated', data => {
       this.setNodes(data);
-    })
+    });
 
     this.socket.on('route deleted', data => {
       this.setNodes(data);
-    })
+    });
 
     this.get = this.get.bind(this);
+    this.uploadScreenshot = this.uploadScreenshot.bind(this);
     this.downloadScreenshot = this.downloadScreenshot.bind(this);
     this.takeScreenshot = this.takeScreenshot.bind(this);
     this.handleNewNode = this.handleNewNode.bind(this);
@@ -84,10 +86,33 @@ class Canvas extends Component {
     });
   }
 
+  uploadScreenshot() {
+    //get the PNG URL to generate a snapshot of the page
+    let imageURL = this.takeScreenshot();
+
+    const options = {
+      method: 'POST',
+      url: '/api/uploadScreenshot',
+      data: {
+        canvasID: window.location.href.split('/canvas/')[1],
+        image: imageURL
+      }
+    };
+
+    axios(options)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        // Actually show user what went wrong
+        console.log(err);
+      });
+  }
+
   downloadScreenshot() {
     //get the PNG URL to generate a snapshot of the page
     let imageURL = this.takeScreenshot();
-    console.log(imageURL);
+    // console.log(imageURL);
     //create a new anchor to hold the image and download event
     var a = window.document.createElement('a');
 
@@ -122,7 +147,6 @@ class Canvas extends Component {
   }
 
   handleNodeMove(data, cb) {
-
     data.room = this.props.match.params.name;
     // console.log(`dummy output:`);
     // console.log(data);
@@ -154,44 +178,50 @@ class Canvas extends Component {
   render() {
     const showNodes = this.state.nodes.map(node => {
       if (this.get(node, 'type') === 'SERVER') {
-        return <Server 
-          get={this.get}
-          routes={this.get(node, 'routes')}
-          id={this.get(node, 'id')}
-          key={this.get(node, 'id')}
-          x={this.get(node, 'x')}
-          y={this.get(node, 'y')}
-          handleMovement={this.handleNodeMove}
-          handleNewRoute={this.handleNewNodeRoute}
-          handleDelete={this.handleNodeDelete}
-        />
+        return (
+          <Server
+            get={this.get}
+            routes={this.get(node, 'routes')}
+            id={this.get(node, 'id')}
+            key={this.get(node, 'id')}
+            x={this.get(node, 'x')}
+            y={this.get(node, 'y')}
+            handleMovement={this.handleNodeMove}
+            handleNewRoute={this.handleNewNodeRoute}
+            handleDelete={this.handleNodeDelete}
+          />
+        );
       } else if (this.get(node, 'type') === 'DATABASE') {
-        return <Database 
-          get={this.get}
-          routes={this.get(node, 'routes')}
-          id={this.get(node, 'id')}
-          key={this.get(node, 'id')}
-          x={this.get(node, 'x')}
-          y={this.get(node, 'y')}
-          handleMovement={this.handleNodeMove}
-          handleNewRoute={this.handleNewNodeRoute}
-          handleDelete={this.handleNodeDelete}
-        />
+        return (
+          <Database
+            get={this.get}
+            routes={this.get(node, 'routes')}
+            id={this.get(node, 'id')}
+            key={this.get(node, 'id')}
+            x={this.get(node, 'x')}
+            y={this.get(node, 'y')}
+            handleMovement={this.handleNodeMove}
+            handleNewRoute={this.handleNewNodeRoute}
+            handleDelete={this.handleNodeDelete}
+          />
+        );
       } else {
-        return <Client
-          get={this.get}
-          routes={this.get(node, 'routes')}
-          id={this.get(node, 'id')}
-          key={this.get(node, 'id')}
-          x={this.get(node, 'x')}
-          y={this.get(node, 'y')}
-          handleMovement={this.handleNodeMove}
-          handleNewRoute={this.handleNewNodeRoute}
-          handleDelete={this.handleNodeDelete}
-        />
-      };
+        return (
+          <Client
+            get={this.get}
+            routes={this.get(node, 'routes')}
+            id={this.get(node, 'id')}
+            key={this.get(node, 'id')}
+            x={this.get(node, 'x')}
+            y={this.get(node, 'y')}
+            handleMovement={this.handleNodeMove}
+            handleNewRoute={this.handleNewNodeRoute}
+            handleDelete={this.handleNodeDelete}
+          />
+        );
+      }
     });
-    
+
     return (
       <div>
         <h2>Shark.io</h2>
@@ -203,10 +233,41 @@ class Canvas extends Component {
             {showNodes}
           </svg>
           <div className="tool-bar">
-            <button onClick={() => this.handleNewNode({ position: { x: 20, y: 20 }, type: 'CLIENT' })}> Client +</button>
-            <button onClick={() => this.handleNewNode({ position: { x: 250, y: 20 }, type: 'SERVER' })}> Server +</button>
-            <button onClick={() => this.handleNewNode({ position: { x: 350, y: 20 }, type: 'DATABASE' })}> Database +</button>
+            <button
+              onClick={() =>
+                this.handleNewNode({
+                  position: { x: 20, y: 20 },
+                  type: 'CLIENT'
+                })
+              }
+            >
+              {' '}
+              Client +
+            </button>
+            <button
+              onClick={() =>
+                this.handleNewNode({
+                  position: { x: 250, y: 20 },
+                  type: 'SERVER'
+                })
+              }
+            >
+              {' '}
+              Server +
+            </button>
+            <button
+              onClick={() =>
+                this.handleNewNode({
+                  position: { x: 350, y: 20 },
+                  type: 'DATABASE'
+                })
+              }
+            >
+              {' '}
+              Database +
+            </button>
             <button onClick={this.downloadScreenshot}> Save Canvas </button>
+            <button onClick={this.uploadScreenshot}> Upload Canvas </button>
           </div>
         </div>
       </div>
--- server/routes.js
+++ server/routes.js
@@ -5,9 +5,23 @@ const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
+router.post('/uploadScreenshot', async (req, res) => {
+  try {
+    const canvas = await db.addImage({
+      canvasID: req.body.canvasID,
+      image: req.body.image
+    });
+    res.send('screenshot added!');
+  } catch (err) {
+    console.log(err);
+    res.statusMessage = err.message;
+    res.sendStatus(err.code);
+  }
+});
+
 router.post('/signup', async (req, res) => {
   req.body.id = uuidv4();
-  
+
   try {
     const password = await bcrypt.hash(req.body.password, 10);
     req.body.password = password;
@@ -15,7 +29,7 @@ router.post('/signup', async (req, res) => {
 
     req.session.userID = user.get('u.id');
     res.end();
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     res.statusMessage = 'That email address is already in use';
     res.sendStatus(403);
@@ -25,19 +39,22 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const user = await db.getUser(req.body);
-    const valid = await bcrypt.compare(req.body.password, user.get('u.password'));
-    
+    const valid = await bcrypt.compare(
+      req.body.password,
+      user.get('u.password')
+    );
+
     if (!valid) {
       throw {
         message: 'Incorrect password',
         code: 401
       };
     }
-    
+
     req.session.userID = user.get('u.id');
 
     res.end();
-  } catch(err) {
+  } catch (err) {
     res.statusMessage = err.message;
     res.sendStatus(err.code);
   }
@@ -49,7 +66,7 @@ router.post('/logout', (req, res) => {
   });
 });
 
-router.post('/canvas/add', async (req, res) => {  
+router.post('/canvas/add', async (req, res) => {
   const canvasID = uuidv4();
 
   try {
@@ -59,9 +76,13 @@ router.post('/canvas/add', async (req, res) => {
         code: 401
       };
     }
-    const canvas = await db.addCanvas({userID: req.session.userID, canvasID, canvasName: req.body.name});
-    res.send({id: canvas.get('c.id'), name: canvas.get('c.name')});
-  } catch(err) {
+    const canvas = await db.addCanvas({
+      userID: req.session.userID,
+      canvasID,
+      canvasName: req.body.name
+    });
+    res.send({ id: canvas.get('c.id'), name: canvas.get('c.name') });
+  } catch (err) {
     console.log(err);
     res.statusMessage = err.message;
     res.sendStatus(err.code);
@@ -79,11 +100,11 @@ router.get('/canvases', async (req, res) => {
 
     const canvases = await db.getUserCanvases(req.session.userID);
     res.send(canvases);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     res.statusMessage = err.message;
     res.sendStatus(err.code);
   }
-})
+});
 
 module.exports = router;
