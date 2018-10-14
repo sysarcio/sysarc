a59d37601110a4d819c04670bbab43e10e080bd4
Navira <30273156+navira5@users.noreply.github.com>
2018-10-14 10:37:24 -0500

    Feat/frontend design and delete canvas functionality (#69)
    * style canvases boards and add onhover hand
    
    * formatted canvases view to match wireframe theme
    
    * delete canvas functionality added
    
    * added onhover on delete button
    
    * fix bug with delete canvas button

--- client/src/components/Canvases.jsx
+++ client/src/components/Canvases.jsx
@@ -7,12 +7,19 @@ class Canvases extends Component {
     super(props);
     this.state = {
       canvases: [],
-      text: ''
+      text: '',
+      showForm: false,
+      showDelete: false
     }
 
+    this.get = this.get.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.createCanvas = this.createCanvas.bind(this);
     this.goToCanvas = this.goToCanvas.bind(this);
+    this.toggleShowForm = this.toggleShowForm.bind(this);
+    this.deleteCanvas = this.deleteCanvas.bind(this);
+    this.toggleShowDelete = this.toggleShowDelete.bind(this)
+
   }
 
   async componentDidMount() {
@@ -23,14 +30,15 @@ class Canvases extends Component {
 
     try {
       const {data: canvases} = await axios(options);
-      console.log(canvases);
+      console.log('canvases from axios', canvases);
       this.setState({
         canvases
       });
+      
     } catch(err) {
       console.log(err);
       // tell user they must be logged in
-      this.props.history.push('/login');
+      // this.props.history.push('/login');
     }
   }
 
@@ -45,43 +53,97 @@ class Canvases extends Component {
     });
   }
 
-  async createCanvas() {
+  async createCanvas(e) {
+    e.preventDefault();
     const options = {
       method: 'POST',
       url: '/api/canvas/add',
       data: {
         name: this.state.text
       }
+     
     };
 
     try {
       const {data: {id, name}} = await axios(options);
+      console.log('id-->', id)
       this.props.history.push(`/canvas/${id}`);
     } catch(err) {
       // Actually let user know that something went wrong
       console.log(err);
     }
+
+    this.toggleShowForm();
   }
 
   goToCanvas(id) {
     this.props.history.push(`/canvas/${id}`);
   }
 
+  toggleShowForm() {
+    this.setState({ showForm: !this.state.showForm})
+  }
+
+deleteCanvas(id) {
+  console.log('got request to delete this-->', id)
+  axios.delete(`/api/canvas/${id}`)
+    .then(response => {
+      console.log('deletion complete-->', response)
+      const canvasesCopy = this.state.canvases.slice();
+     
+      const canvasesAfterDelete = canvasesCopy.filter(canvas => {
+        return this.get(canvas, 'id') !== id;
+      })
+
+      this.setState({
+        canvases: canvasesAfterDelete 
+      });
+    })
+    .catch(err => {
+      console.log('unable to delete canvas ', err);
+    })
+  }
+
+  toggleShowDelete() {
+    this.setState({
+      showDelete: !this.showDelete
+    })
+  }
+
   render() {
-    return (
-      <div>
+
+    const showCreateNewCanvas = this.state.showForm ? 
+      <div >
+      <form className='canvases-form' >
         <input
           type="text"
           placeholder="Canvas name..."
           onChange={this.handleChange}
           value={this.state.text}
+          className='form-input'
         />
-        <button onClick={this.createCanvas}>Create Canvas</button>
-        <div className="canvases">
+          <button className='form-button' onClick={this.createCanvas}>+</button>
+        </form> 
+    </div> : 
+      
+      <div  className='canvases-plus'>
+        <button className='canvas-button' onClick={this.toggleShowForm} >
+          <strong>+</strong>
+        </button>
+      </div>
+
+    return (
+      <div className='canvases '>
+       
+          {showCreateNewCanvas}
+
           {this.state.canvases.map(c => {
             if (this.get(c, 'id')) {
               return (
                 <CanvasThumbnail
+                  showDelete={this.state.showDelete}
+                  toggleShowDelete={this.toggleShowDelete}
+                  deleteCanvas={this.deleteCanvas}
                   get={this.get}
                   canvas={c}
                   goToCanvas={this.goToCanvas}
@@ -90,7 +152,8 @@ class Canvases extends Component {
               )
             }
           })}
-        </div>
+
+   
       </div>
     )
   }
