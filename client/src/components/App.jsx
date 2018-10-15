35e6fc1fdb75c8b817e7e6fb9265b737f36190a5
Jonathan Tredway <jptredway@gmail.com>
2018-10-15 12:05:16 -0500

    Structure state to mirror swagger

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        {/* <NavBar /> */}
         <Route
           exact path='/'
           component={Canvases}
