9801f9f4db4f4b56551437a47290ba25bfa2a342
navira5 <navira.ali@gmail.com>
2018-10-15 15:35:56 -0500

    show delete confirmation on delete button

--- client/src/components/CanvasThumbnail.jsx
+++ client/src/components/CanvasThumbnail.jsx
@@ -1,35 +1,67 @@
-import React from 'react';
-import DeletePop from './DeletePop.jsx'
+import React, { Component } from 'react';
+import DeletePop from './DeletePop.jsx';
 
+  
+// const CanvasThumbnail = ({ canvas, get, goToCanvas, deleteCanvas}) => {
 
-const CanvasThumbnail = ({ canvas, get, goToCanvas, deleteCanvas}) => {
+class CanvasThumbnail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    }
 
-  return (     
+    this.handleModalShowClick = this.handleModalShowClick.bind(this);
+    this.handleModalCloseClick = this.handleModalCloseClick.bind(this);
+  }
+
+  handleModalShowClick(e) {
+    e.preventDefault();
+    this.setState({
+      showModal: true
+    })
+  }
+
+  handleModalCloseClick() {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  render() {
+
+    const showModal = this.state.showModal;
+    const showDeleteOption = <button className='canvas-delete-btn' onClick={this.handleModalShowClick}>X</button>
+    const showConfirmation = <div> Delete Canvas? <button onClick={() => this.props.deleteCanvas(this.props.get(this.props.canvas, 'id'))}> Delete </button> <button onClick={this.handleModalCloseClick}> Cancel </button></div>
+
+    return (
       <div
         className="canvas-thumbnail"
-        id={get(canvas, 'id')}>
-      <button className='canvas-delete-btn' onClick={() => deleteCanvas(get(canvas, 'id'))}>
-        <span>Delete Canvas</span>
-        X
-        </button>
-       <div>
-        <h3 onClick={() => goToCanvas(get(canvas, 'id'))}>{get(canvas, 'name')}</h3>
-       
-        <img
-          onClick={() => goToCanvas(get(canvas, 'id'))}
-          src={get(canvas, 'image')}
-          height="40"
-          width="160"
-          alt={get(canvas, 'name')}
-        />
-        <pre>{get(canvas, 'id')}</pre>
-      </div>; 
+        id={this.props.get(this.props.canvas, 'id')}>
+        
+        {showModal ? showConfirmation : showDeleteOption }
+        <div>
+          <h3 onClick={() => this.props.goToCanvas(this.props.get(this.props.canvas, 'id'))}>{this.props.get(this.props.canvas, 'name')}</h3>
+
+          <img
+            onClick={() => this.props.goToCanvas(this.props.get(this.props.canvas,  'id'))}
+            src={this.props.get(this.props.canvas, 'image')}
+            height="40"
+            width="160"
+            alt={this.props.get(this.props.canvas, 'name')}
+          />
+          <pre>{this.props.get(this.props.canvas, 'id')}</pre>
+        </div>;
       </div>
 
-    
-  );
 
+    );
+  }
 }
+  
   
 
 export default CanvasThumbnail;
+
+// onClick = {() => deleteCanvas(get(canvas, 'id'))}
+//onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) deleteCanvas(get(canvas, 'id')) }}
--- client/src/components/Canvases.jsx
+++ client/src/components/Canvases.jsx
@@ -2,14 +2,52 @@ import React, { Component } from 'react';
 import CanvasThumbnail from './CanvasThumbnail.jsx';
 import axios from 'axios';
 
+
+
+// class Canvases extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleModalShowClick = this.handleModalShowClick.bind(this);
+//     this.handleModalCloseClick = this.handleModalCloseClick.bind(this);
+//     this.state = {
+//       showModal: false,
+//     }
+//   }
+
+//   handleModalShowClick(e) {
+//     e.preventDefault();
+//     this.setState({
+//       showModal: true
+//     })
+//   }
+
+//   handleModalCloseClick() {
+//     this.setState({
+//       showModal: false
+//     })
+//   }
+
+//   render() {
+//     console.log(this.state)
+//     const { showModal } = this.state;
+//     return (
+//       <div className="container">
+//         <div className="row">
+//           <div className="col text-center"><button type="button" className="btn btn-primary" onClick={this.handleModalShowClick}>Show Modal</button></div>
+//           {showModal ? (<Modal handleModalCloseClick={this.handleModalCloseClick} />) : null}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
 class Canvases extends Component {
   constructor(props) {
     super(props);
     this.state = {
       canvases: [],
       text: '',
-      showForm: false,
-      showDelete: false
+      showForm: false,   
     }
 
     this.get = this.get.bind(this);
@@ -18,8 +56,7 @@ class Canvases extends Component {
     this.goToCanvas = this.goToCanvas.bind(this);
     this.toggleShowForm = this.toggleShowForm.bind(this);
     this.deleteCanvas = this.deleteCanvas.bind(this);
-    this.toggleShowDelete = this.toggleShowDelete.bind(this)
-
+  
   }
 
   async componentDidMount() {
@@ -84,30 +121,20 @@ class Canvases extends Component {
     this.setState({ showForm: !this.state.showForm})
   }
 
-deleteCanvas(id) {
-  console.log('got request to delete this-->', id)
-  axios.delete(`/api/canvas/${id}`)
-    .then(response => {
-      console.log('deletion complete-->', response)
-      const canvasesCopy = this.state.canvases.slice();
-     
-      const canvasesAfterDelete = canvasesCopy.filter(canvas => {
-        return this.get(canvas, 'id') !== id;
+  deleteCanvas(id) {
+    axios.delete(`/api/canvas/${id}`)
+      .then(response => {
+        const canvasesCopy = this.state.canvases.slice();
+        const canvasesAfterDelete = canvasesCopy.filter(canvas => {
+          return this.get(canvas, 'id') !== id;
       })
-
-      this.setState({
-        canvases: canvasesAfterDelete 
-      });
-    })
-    .catch(err => {
-      console.log('unable to delete canvas ', err);
-    })
-  }
-
-  toggleShowDelete() {
-    this.setState({
-      showDelete: !this.showDelete
-    })
+        this.setState({
+          canvases: canvasesAfterDelete 
+        });
+      })
+      .catch(err => {
+        console.log('unable to delete canvas ', err);
+      })
   }
 
   render() {
@@ -141,8 +168,7 @@ deleteCanvas(id) {
             if (this.get(c, 'id')) {
               return (
                 <CanvasThumbnail
-                  showDelete={this.state.showDelete}
-                  toggleShowDelete={this.toggleShowDelete}
+               
                   deleteCanvas={this.deleteCanvas}
                   get={this.get}
                   canvas={c}
