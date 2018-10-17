548e83567834876f59abdce7e8e9ea0f1bf72b02
navira5 <navira.ali@gmail.com>
2018-10-17 16:25:21 -0500

    logo animation complete

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,7 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
-      
-       {
-         test: /\.(png|jpg)$/,
-         loader: 'url-loader?limit=8192'
-       }
-        
-      
-    ],
+    
     rules : [
       {
         test : /\.jsx?/,
