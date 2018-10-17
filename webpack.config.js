93b6848612d8ecfde10d55d65af1290a25170366
Michael Thomasson <mjthomasson@gmail.com>
2018-10-17 14:59:53 -0500

    made minor changes to webpack config. add script and remove canvg, style-components, and ioredis

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+  mode: "production", // enable many optimizations for production builds
+  mode: "development", // enabled useful tools for development
+  mode: "none",
+  plugins: [
+    new CompressionPlugin()
+  ],
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -20,5 +27,16 @@ module.exports = {
        }
       }
     ]
+  },
+  performance: {
+    hints: "warning", // enum
+    hints: "error", // emit errors for perf hints
+    hints: false, // turn off perf hints
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function(assetFilename) {
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
   }
 };
