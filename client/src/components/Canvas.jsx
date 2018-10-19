6cc3ca4597ad77f32e028529496a184ac729b228
Tredway91 <Tredway91@users.noreply.github.com>
2018-10-18 22:08:15 -0500

    Style/header bar (#110)
    * Implement multi-point lines
    
    * Line up canvas name with nav links

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -427,9 +427,15 @@ class Canvas extends Component {
             Sketchpad Ninja
           </h1>
         </div>
+<<<<<<< HEAD
         <div className="canvas-name">
           {/* CANVAS NAME */}
           <h2>{this.state.name}</h2>
+        </div>
+        <div className="header-links">
+            <p className='logout-p' onClick={this.logout}>Logout</p>
+            <p className='canvases-p' onClick={this.goToCanvases}> Canvases </p>
+          <p className='canvases-p' onClick={() => this.processScreenshot('DOWNLOAD')}>Screenshot</p>
         </div>
         <div className="header-links">
           <p className="logout-p" onClick={this.logout}>
@@ -448,6 +454,30 @@ class Canvas extends Component {
           >
             Screenshot
           </p>
+=======
+        <div className="header-bar">
+          <div>
+            <h2>{this.state.name}</h2>
+          </div>
+          <div>
+            <p className="logout-p" onClick={this.logout}>
+              Logout
+            </p>
+            <p className="canvases-p" onClick={this.goToCanvases}>
+              {' '}
+              Canvases{' '}
+            </p>
+            <p className='canvases-p' onClick={this.takeMeToTheDocs}>
+              Docs
+            </p>
+            <p
+              className="canvases-p"
+              onClick={() => this.processScreenshot('DOWNLOAD')}
+            >
+              Screenshot
+            </p>
+          </div>
+>>>>>>> Line up canvas name with nav links
         </div>
         <div style={{ overflow: 'hidden' }}>
           <div id="canvas" width={this.state.width} height={this.state.height}>
