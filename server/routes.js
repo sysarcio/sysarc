d0ffe1f0dfe7d9eeedc8cb26f0a5f43c038bf22e
navira5 <navira.ali@gmail.com>
2018-10-19 12:14:10 -0500

    resolve merge conflicts

--- server/routes.js
+++ server/routes.js
@@ -140,8 +140,6 @@ router.get('/getRoomData/:id', async (req, res) => {
     
     res.send({nodes, connections, name});
   } catch(err) {
-<<<<<<< HEAD
-=======
     console.log(err);
   }
 });
@@ -168,7 +166,6 @@ router.get('/Docs/:id', async (req, res) => {
     // console.log('leaving server with records: ', records)
     res.send({connections});
     } catch(err) {
->>>>>>> 6be7715abc10fed59b8e78b1470f9a8b6e86667b
     console.log(err);
   }
 });
